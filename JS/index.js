3ef6dec31a7b79e884f803ee1a5a9aba813baf23
PatrickP595243 <47600251+PatrickP595243@users.noreply.github.com>
2019-06-14 10:11:01 -0400

    Add files via upload

--- JS/index.js
+++ JS/index.js
@@ -18,14 +18,3 @@ function announcements(){
 	counter += 1
 	ctx.fillText(x,5,(25 * counter));
 }
-
-function clear(){
-	var c = document.getElementById("myCanvas2");
-	var ctx = c.getContext("2d");
-	ctx.font = "20px Arial";
-	ctx.fillText("Announcements",5,25);
-	ctx.font = "16px Arial";
-	ctx.fillStyle = "red"
-	ctx.fillRect(0,0,300,150);
-
-}
